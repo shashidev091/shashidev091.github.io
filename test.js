cf632e5a6cca09cf444fb0211714f845ab259995
Shashi Bhagat <skujur871@gmail.com>
2021-03-22 13:54:10 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+function encodeImageFileAsURL(element) {
+    var file = element.files[0];
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      console.log('RESULT', reader.result)
+    }
+    reader.readAsDataURL(file);
+  }
+
+  
